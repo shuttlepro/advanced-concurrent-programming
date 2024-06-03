6faa1204b72f45ff56a031dbfbe558650e1bae19
Shuttle <Shuttleupup@163.com>
2024-06-03 23:55:42 +0800

    feat: Add deadLock demo and solutions

--- src/main/java/com/shuttle/acp/deadlock/solution/SolutionDeadLockMonitor.java
+++ src/main/java/com/shuttle/acp/deadlock/solution/SolutionDeadLockMonitor.java
@@ -1,6 +1,6 @@
 package com.shuttle.acp.deadlock.solution;
 
-import com.shuttle.acp.deadlock.SleepUtils;
+import com.shuttle.acp.utils.SleepUtils;
 import lombok.extern.slf4j.Slf4j;
 
 import java.lang.management.ManagementFactory;
